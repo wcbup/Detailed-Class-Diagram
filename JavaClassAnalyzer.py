a2c77733635b20d749b2806183bcacaccded6e81
wcbup <wcbup123@foxmail.com>
2023-09-15 20:39:13 +0200

    show field content in the graph

--- JavaClassAnalyzer.py
+++ JavaClassAnalyzer.py
@@ -5,23 +5,21 @@ import json
 import os
 
 
-class JavaInterface:
-    """
-    class representing the java interface
-    """
-
-    def __init__(self, id: str) -> None:
-        self.id = id
+class JavaClass:
+    def __init__(self, type_id: str, name: str | None, is_base_type: bool) -> None:
+        self.type_id = type_id
+        self.name = name
+        self.is_base_type = is_base_type
         self.arg_ids: List[str] = []
 
     def add_one_arg_id(self, id: str) -> None:
         self.arg_ids.append(id)
 
-    def get_detailed_id(self) -> str:
+    def get_detailed_type_id(self) -> str:
         if len(self.arg_ids) == 0:
-            return self.id
+            return self.type_id
         else:
-            return f"{self.id}&lt;{', '.join(self.arg_ids)}&gt;"
+            return f"{self.type_id}&lt;{', '.join(self.arg_ids)}&gt;"
 
 
 class JavaClassAnalyzer:
@@ -46,12 +44,11 @@ class JavaClassAnalyzer:
         self.super_class_id = get_id(self.content_json["super"]["name"])
 
         # realization
-
-        self.interface_set: Set[JavaInterface] = set()
+        self.interface_set: Set[JavaClass] = set()
         interfaces_content: List[Dict[str]] = self.content_json["interfaces"]
         for interface_content in interfaces_content:
             interface_id = get_id(interface_content["name"])
-            java_interface = JavaInterface(interface_id)
+            java_interface = JavaClass(interface_id, None, False)
 
             args_content: List[Dict[str]] = interface_content["args"]
             for arg_content in args_content:
@@ -59,6 +56,25 @@ class JavaClassAnalyzer:
                 java_interface.add_one_arg_id(arg_id)
 
             self.interface_set.add(java_interface)
+
+        # aggregation
+        self.field_set: Set[JavaClass] = set()
+        fields_content: List[Dict[str]] = self.content_json["fields"]
+        for field_content in fields_content:
+            name = field_content["name"]
+
+            type_content: Dict[str] = field_content["type"]
+            if "name" in type_content.keys():
+                type_id = get_id(field_content["type"]["name"])
+                args_content: List[Dict[str]] = field_content["type"]["args"]
+                java_class = JavaClass(type_id, name, False)
+                for arg_content in args_content:
+                    java_class.add_one_arg_id(get_id(arg_content["type"]["name"]))
+            else:
+                type_id = field_content["type"]["base"]
+                java_class = JavaClass(type_id, name, True)
+
+            self.field_set.add(java_class)
 
 
 class ClassPainter:
@@ -92,15 +108,15 @@ class ClassPainter:
     """
         self.dot_id = 0  # the id for java class in dot class
 
-        def allocate_id(java_class: str | JavaClassAnalyzer | JavaInterface) -> None:
+        def allocate_class_id(java_class: str | JavaClassAnalyzer | JavaClass) -> None:
             """
             allocate the id to java class if it doesn't have one
             """
-            if isinstance(java_class, str) or isinstance(java_class, JavaInterface):
+            if isinstance(java_class, str) or isinstance(java_class, JavaClass):
                 if isinstance(java_class, str):
                     java_class_id = java_class
-                elif isinstance(java_class, JavaInterface):
-                    java_class_id = java_class.get_detailed_id()
+                elif isinstance(java_class, JavaClass):
+                    java_class_id = java_class.get_detailed_type_id()
                 if java_class_id in dot_id_map:
                     return
                 else:
@@ -137,6 +153,11 @@ class ClassPainter:
                     dot_id_map[java_class_id] = str(self.dot_id)
                     dot_id = self.dot_id
                     self.dot_id += 1
+
+                    field_code = ""
+                    for field_class in java_class.field_set:
+                        field_code += f'<tr> <td port="{field_class.name}" align="left" >- {field_class.name}: {field_class.get_detailed_type_id()}</td> </tr>'
+
                     self.dot_code += f"""
                         x{dot_id} [
                             shape=plain
@@ -145,9 +166,7 @@ class ClassPainter:
                                 <tr> <td>
                                     <table border="0" cellborder="0" cellspacing="0" >
                                         <tr> <td align="left" >+ field</td> </tr>
-                                        <tr> <td port="ss1" align="left" >- Subsystem 1</td> </tr>
-                                        <tr> <td port="ss2" align="left" >- Subsystem 2</td> </tr>
-                                        <tr> <td port="ss3" align="left" >- Subsystem 3</td> </tr>
+                                        {field_code}
                                     </table>
                                 </td> </tr>
                                 <tr> <td>
@@ -163,13 +182,13 @@ class ClassPainter:
 
         # allocate the dot id to java class analyzer
         for java_class in self.java_class_set:
-            allocate_id(java_class)
+            allocate_class_id(java_class)
         for java_class in self.java_class_set:
-            allocate_id(java_class.super_class_id)
+            allocate_class_id(java_class.super_class_id)
             for java_interface in java_class.interface_set:
-                allocate_id(java_interface)
+                allocate_class_id(java_interface)
                 for arg_id in java_interface.arg_ids:
-                    allocate_id(arg_id)
+                    allocate_class_id(arg_id)
 
         for java_class in self.java_class_set:
             dot_id = dot_id_map[java_class.id]
@@ -181,7 +200,7 @@ class ClassPainter:
             for interface in java_class.interface_set:
                 self.dot_code += f"""
                         edge [arrowhead=empty style=dashed]
-                        x{dot_id} -> x{dot_id_map[interface.get_detailed_id()]}
+                        x{dot_id} -> x{dot_id_map[interface.get_detailed_type_id()]}
     """
 
         self.dot_code += "}"
@@ -213,8 +232,6 @@ if __name__ == "__main__":
     java_analyzer_list: List[JavaClassAnalyzer] = []
     for content_str in content_str_list:
         java_analyzer_list.append(JavaClassAnalyzer(content_str))
-
-    print(java_analyzer_list[0].id)
 
     class_painter = ClassPainter(java_analyzer_list)
     class_painter.generate_graph_and_show()
